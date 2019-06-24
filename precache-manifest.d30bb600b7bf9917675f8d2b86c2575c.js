9ad5485f229e786827d60fc34c7bb1139df104e4
Subhan Ahmed <subhanahmed047@gmail.com>
2019-06-24 15:52:13 +1000

    Updates

---
+++ precache-manifest.d30bb600b7bf9917675f8d2b86c2575c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d3041703a36cd55e9f326d62276e5f",
+    "url": "/pizza-canvas/index.html"
+  },
+  {
+    "revision": "83537c33e7593cb07ff1",
+    "url": "/pizza-canvas/static/css/2.ab7e5612.chunk.css"
+  },
+  {
+    "revision": "95bbd58b07cb7c2872cb",
+    "url": "/pizza-canvas/static/css/main.a45524ee.chunk.css"
+  },
+  {
+    "revision": "83537c33e7593cb07ff1",
+    "url": "/pizza-canvas/static/js/2.3c926367.chunk.js"
+  },
+  {
+    "revision": "95bbd58b07cb7c2872cb",
+    "url": "/pizza-canvas/static/js/main.3bfa5b0e.chunk.js"
+  },
+  {
+    "revision": "408a9f71b9de1f2c1755",
+    "url": "/pizza-canvas/static/js/runtime~main.8a2a5a3b.js"
+  }
+]);
